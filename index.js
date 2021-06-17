e43abb1b035dbecbca509292ee77aeb418413151
lakshy pachory <luckyron1279@gmail.com>
2021-06-17 23:01:04 +0530

    song_upload_backend

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const post = require("./model/post");
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://anshita:pipdIq5fqHCYrgO7@cluster0.adtj0.mongodb.net/codeial_production?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.get("/", (req, res) => {
+  post
+    .find()
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.post("/upload", (req, res) => {
+  const { movieName, releaseDate, thumbnail, language, video } = req.body;
+  console.log(req.body);
+  if (!movieName || !releaseDate || !thumbnail || !language || !video) {
+    return res.status(422).json({ error: "please prvide all filed" });
+  }
+  const Post = new post({
+    movieName,
+    releaseDate,
+    thumbnail,
+    language,
+    video,
+  });
+  Post.save()
+    .then((result) => {
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongodb");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("error in connected to mongodb", err);
+});
+app.listen(PORT, () => {
+  console.log("server is running on", PORT);
+});
---
+++ model/post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  movieName: {
+    type: String,
+    require: true,
+  },
+  releaseDate: {
+    type: String,
+    require: true,
+  },
+  thumbnail: {
+    type: String,
+    require: true,
+  },
+  language: {
+    type: String,
+    require: true,
+  },
+  video: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("Post", postSchema);
